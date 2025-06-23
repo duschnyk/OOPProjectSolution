65e64538aee1d0ee9341d49600017bdf91a07cb7
duschnyk <44duscniyk11@gmail.com>
2025-06-23 21:46:48 +0300

    Constructor and destructor add

--- mainProject/MusicalInstrument.h
+++ mainProject/MusicalInstrument.h
@@ -7,7 +7,7 @@ public:
 	string typeOfMusicalInstrument;
 	string brand;
 	string model;
-	string pickups;
+	string modelOfPickups;
 	string materialOfBody;
 	string materialOfNeck;
 	string materialOfFretboard;
@@ -19,11 +19,12 @@ public:
 	int amountInOffer;
 	string dateOfRelease;
 
+	//default constructor
 	MusicalInstrument() {
 		typeOfMusicalInstrument = "instrument not found...";
 		brand = "none";
 		model = "none";
-		pickups = "none";
+		modelOfPickups = "none";
 		materialOfBody = "none";
 		materialOfNeck = "none";
 		materialOfFretboard = "none";
@@ -35,12 +36,71 @@ public:
 		amountInOffer = 0;
 		dateOfRelease = "no date";
 	}
+	//constructor with arguments
+	MusicalInstrument(string typeOfMusicalInstrument, string brand, string model, bool isInOffer, int amountInOffer) {
+		this->typeOfMusicalInstrument = typeOfMusicalInstrument;
+		this->brand = brand;
+		this->model = model;
+		modelOfPickups = "none";
+		materialOfBody = "none";
+		materialOfNeck = "none";
+		materialOfFretboard = "none";
+		modelOfBridge = "none";
+		amountOfStrings = 0;
+		amountOfPickups = 0;
+		price = 0;
+		this->isInOffer = isInOffer;
+		this->amountInOffer = amountInOffer;
+		dateOfRelease = "no date";
+	}
+
+	//canonical constructor
+	MusicalInstrument(string typeOfMusicalInstrument, string brand, string model,
+		string modelOfPickups, string materialOfBody, string materialOfNeck,
+		string materialOfFretboard, string modelOfBridge, int amountOfStrings,
+		int amountOfPickups, bool price, bool isInOffer, int amountInOffer,
+		string dateOfRelease) {
+		this->typeOfMusicalInstrument = typeOfMusicalInstrument;
+		this->brand = brand;
+		this->model = model;
+		this->modelOfPickups = modelOfPickups;
+		this->materialOfBody = materialOfBody;
+		this->materialOfNeck = materialOfNeck;
+		this->materialOfFretboard = materialOfFretboard;
+		this->modelOfBridge = modelOfBridge;
+		this->amountOfStrings = amountOfStrings;
+		this->amountOfPickups = amountOfPickups;
+		this->price = price;
+		this->isInOffer = isInOffer;
+		this->amountInOffer = amountInOffer;
+		this->dateOfRelease = dateOfRelease;
+	}
+
+	//copy constructor
+	MusicalInstrument(const MusicalInstrument& musicalInstrument) {
+		this->typeOfMusicalInstrument = musicalInstrument.typeOfMusicalInstrument;
+		this->brand = musicalInstrument.brand;
+		this->model = musicalInstrument.model;
+		this->modelOfPickups = musicalInstrument.modelOfPickups;
+		this->materialOfBody = musicalInstrument.materialOfBody;
+		this->materialOfNeck = musicalInstrument.materialOfNeck;
+		this->materialOfFretboard = musicalInstrument.materialOfFretboard;
+		this->modelOfBridge = musicalInstrument.modelOfBridge;
+		this->amountOfStrings = musicalInstrument.amountOfStrings;
+		this->amountOfPickups = musicalInstrument.amountOfPickups;
+		this->price = musicalInstrument.price;
+		this->isInOffer = musicalInstrument.isInOffer;
+		this->amountInOffer = musicalInstrument.amountInOffer;
+		this->dateOfRelease = musicalInstrument.dateOfRelease;
+	}
+
+	~MusicalInstrument() {};
 
 	string toString() {
 		string s = typeOfMusicalInstrument + ", ";
 		s += "brand: " + brand + ", ";
 		s += "model: " + model + ", ";
-		s += "pickups: " + pickups + ", ";
+		s += "pickups: " + modelOfPickups + ", ";
 		s += "materialOfBody: " + materialOfBody + ", ";
 		s += "materialOfNeck: " + materialOfNeck + ", ";
 		s += "materialOfFretboard: " + materialOfFretboard + ", ";
--- mainProject/main.cpp
+++ mainProject/main.cpp
@@ -1,9 +1,20 @@
 #include "MusicalInstrument.h"
 
 int main() {
-	MusicalInstrument electricGuitar1;
+	MusicalInstrument test01;
+	//MusicalInstrument electricGuitar1("Electric Guitar", "IBANEZ", "RGRT421", true, 26);
+	//MusicalInstrument electricGuitar1("Test01", "Test01", "Test01", true, 26);
+	//MusicalInstrument electricGuitar2("Electric Guitar", "FLIGHT", "SUPERSTRAT", false, 0);
+	MusicalInstrument electricGuitar2("Test02", "Test02", "Test02", false, 0);
 
+	MusicalInstrument electricGuitar1("Electric Guitar", "IBANEZ",
+		"RGRT421", "QUANTUM", "NYATOH", "MAPLE/NUT",
+		"JATOBA", "F106", 6, 2, 253.33, true, 26,
+		"21.12.21");
+
+	cout << test01.toString() << endl;
 	cout << electricGuitar1.toString() << endl;
+	cout << electricGuitar2.toString() << endl;
 
 
 	return 0;
